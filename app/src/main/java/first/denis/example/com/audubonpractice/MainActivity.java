34dd1e3e6658cbe866d329802d7f70ca1838db47
Slashthedragon <slashinme@gmail.com>
2016-06-20 14:10:11 -0400

    Added TODO comments

--- app/src/main/java/first/denis/example/com/audubonpractice/MainActivity.java
+++ app/src/main/java/first/denis/example/com/audubonpractice/MainActivity.java
@@ -11,4 +11,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
-//comment
+//TODO public class Bird
+// Give it name, image and song. Is ID added automatically.
+
+//TODO Make an audio player.
+
+//TODO make a data base with the name, image and song
+
+//TODO Use a randomizer to select a bird and put it into the array for the display.
+
+//TODO Use a randomizer to select one of the birds in the display.
+
+//TODO play the selected bird when play button is clicked.
+i
+//TODO select the bird associated to the display button which is clicked.
+
+//TODO Play the selected bird song.
+
+//TODO If the two selected birds are the same, change the border of the bird display mage, increment the counter
+
+//TODO  Remove the bird from the possible selected birds.
+
+//TODO  
+
+
